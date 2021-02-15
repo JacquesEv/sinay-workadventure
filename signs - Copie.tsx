6d3073382452168c002a0a8c3914f82649daba0a
JacquesEv <69908815+JacquesEv@users.noreply.github.com>
2021-02-15 21:34:22 +0000

    Deploying to gh-pages from master @ fed5f7be2e664b369dcb1eb34e5d7806e8762e9e 🚀

---
+++ signs - Copie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="signs - Copie" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="../../Workadventure/tilesets/signs - Copie.png" trans="ffffff" width="256" height="192"/>
+</tileset>
