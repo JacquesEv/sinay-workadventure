705736256b142fd6bb2f911822f6a372c5803191
JacquesEv <69908815+JacquesEv@users.noreply.github.com>
2021-02-15 21:57:53 +0000

    Deploying to gh-pages from master @ 5ae1020e5813a5228dad0e73a6d4e5a5c535763a 🚀

--- signs3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="signs3" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="../../Workadventure/tilesets/signs - Copie (3).png" trans="ffffff" width="256" height="192"/>
-</tileset>
